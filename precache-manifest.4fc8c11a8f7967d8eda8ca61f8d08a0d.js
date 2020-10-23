5e6a73b44b361694473c03eef03f5c0d9ff81908
Oro-boro <FullBlueFM@gmail.com>
2020-10-23 06:30:49 +0200

    Updates

---
+++ precache-manifest.4fc8c11a8f7967d8eda8ca61f8d08a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076cf56225862e38b251929feb3807f9",
+    "url": "/Oro-boro/projet_vente.git/index.html"
+  },
+  {
+    "revision": "559849fb3487efea83c4",
+    "url": "/Oro-boro/projet_vente.git/static/css/main.e21a0b48.chunk.css"
+  },
+  {
+    "revision": "9a3c7954bb972159618e",
+    "url": "/Oro-boro/projet_vente.git/static/js/2.47b27569.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Oro-boro/projet_vente.git/static/js/2.47b27569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559849fb3487efea83c4",
+    "url": "/Oro-boro/projet_vente.git/static/js/main.fa6637b5.chunk.js"
+  },
+  {
+    "revision": "fbe4450996c7b01abbcc",
+    "url": "/Oro-boro/projet_vente.git/static/js/runtime-main.23abf74b.js"
+  }
+]);
